e98abccd8be968c7a8fb3b10a8b826ef59ee0392
imit0r <rotimigbafe@gmail.com>
2023-03-09 11:04:03 +0100

    update

---
+++ basic terminologies.rb
@@ -0,0 +1,15 @@
+#puts "hello world"#(returns a string with a nil value as a new line)
+#p 'hello world'#(returns the string 'as is')
+#print "hello world"#(returns the string without adding a new line)
+
+#greeting = "hello imit0r" #(printing statements using variables)
+#puts greeting
+
+
+#(defining a method to print a statement/the result of an arg)
+def say_hello(thing_to_say)
+	puts thing_to_say
+end
+
+say_hello "Hello imit0r"
+
--- coding the quiz.rb
+++ coding the quiz.rb
@@ -1,3 +1,5 @@
+#Creating an address for the sequence of numbers, [1,2,3,4,5,6,7,8,9], printing, indexing and reversing the sequence
+
 address = [1,2,3,4,5,6,7,8,9]
 p address
 p address[5]
