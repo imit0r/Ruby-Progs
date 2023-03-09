35998c79cfc77f998988c91b978232406b18b1f6
imit0r <rotimigbafe@gmail.com>
2023-03-09 12:54:40 +0100

    An older piece of code I did when I touched on Ruby

---
+++ array.rb
@@ -0,0 +1,39 @@
+#{nums = Array.new([1, 2, 3])
+#names = Array.new(["Kofi", "Kola", "Ken"])
+
+#puts "#{names}"}
+
+
+#tmp = Array.new(5, "hellno")
+
+#puts "#{tmp}"
+
+#tmp = Array(20..30) [an array of numerals which includes the last element]
+#tmp = Array(20...30) [an array of numerals which does not include the last element]
+
+#puts "#{tmp}"
+
+#tmp = Array.new(10) { |i|  }
+#puts "#{tmp}"
+
+
+a = " this user is a doodoo"
+b = String.new("doodoo")
+puts a.upcase
+puts a.downcase
+
+
+#grades = Hash.new 
+
+#grades.default = "improvee"
+
+#grades[10] = "A+"
+#grades[9] = "A"
+#grades[8] = "B"
+
+#puts grades[7]
+
+#for i in (1..100000)
+#	puts i
+#	i+=1
+#end
